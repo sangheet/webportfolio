3534f4193f1c33212eef1cde61aec56ed49a7f22
Unknown <norbinski@gmail.com>
2019-05-08 11:24:26 -0500

    08/05-4
    08/05-4

--- dist/js.e31bb0bc.js
+++ dist/js.e31bb0bc.js
@@ -38741,7 +38741,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61144" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57345" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
